77c65314f9c67c17431a86e0626c0250191284a1
Adrian Rodriguez <rodriguezcadr@gmail.com>
2022-10-16 15:07:49 -0300

    doc. funcs. & const.js

---
+++ scripts/const.js
@@ -0,0 +1,3 @@
+const cartMenuContainer = document.querySelector("#cart_menu_container");
+const cartNavIcon = document.querySelector("#cart_nav_icon");
+const closeButton = document.querySelector("#closeButton");
--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,14 @@
-// variables
-const cartMenuContainer = document.querySelector('#cart_menu_container');
-const cartNavIcon = document.querySelector('#cart_nav_icon');
-const closeButton = document.querySelector('#closeButton');
+// Mostrar carrito de compras
+const showCartMenu = () => {
+	cartMenuContainer.style.display = "grid";
+};
+// Ocultar carrito de compras
+const closeCartMenu = () => {
+	cartMenuContainer.style.display = "none";
+};
 
-// funciones
-const showCartMenu = ()=>{
-    cartMenuContainer.style.display="grid";
-}
-const closeCartMenu = ()=>{
-    cartMenuContainer.style.display="none";
-}
-
-// init
-const init = () =>{
-    cartNavIcon.addEventListener('click', showCartMenu);
-    closeButton.addEventListener('click',closeCartMenu);
-}
+const init = () => {
+	cartNavIcon.addEventListener("click", showCartMenu);
+	closeButton.addEventListener("click", closeCartMenu);
+};
 init();
