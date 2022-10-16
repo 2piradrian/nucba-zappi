2db58666c30a628c5040a9d090c63141682d2650
Facundo Villanova <103543642+FacundoVvv@users.noreply.github.com>
2022-10-16 19:44:56 -0300

    filtered de populares (falta render)

--- scripts/index.js
+++ scripts/index.js
@@ -12,11 +12,32 @@ const closeCartMenu = () => {
 	cartMenuContainer.style.display = "none";
 };
 
+//se renderizan los filtrados populares que le llegan de parametro
+const renderMostPopulars = (arrayToRender) =>{
+	console.log(arrayToRender)
+	return `<div class="itemContainer">
+					<img src="/assets/img/pizzas/pizza1.png" alt="imagen del item" srcset="" />
+					<div class="itemDescription">
+							<h3 class="itemTitle">La Mr. Pit</h3>
+							<p class="itemSubtitle">Solo para expertos</p>
+							<div class="itemBuy">
+							<p class="price">$ 350</p>
+							<button class="addToCart">Agregar</button>
+					</div>
+			</div>`
+}
+// se filtran los mas populares
+const filterMostPopulars = (arrayOfObjects)=>{
+	const mostPopularFiltered = arrayOfObjects.filter(e=> e.popular);
+	renderMostPopulars(mostPopularFiltered)
+}
+
 const init = () => {
 	cartNavIcon.addEventListener("click", showCartMenu);
 	closeButton.addEventListener("click", closeCartMenu);
 	document.addEventListener('click',(e)=>{
 		if(!cartMenuContainer.contains(e.target) && e.target !== cartNavIcon){closeCartMenu()};
 	})
+	addEventListener('DOMContentLoaded', filterMostPopulars(productsArray));
 };
 init();
