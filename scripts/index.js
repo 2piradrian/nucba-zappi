e482cfc09dabcde90fcb41b755e58e114862dfa9
Adrian <rodriguezcadr@gmail.com>
2022-10-17 14:28:13 -0300

    cleanPrices & cartRender

--- scripts/index.js
+++ scripts/index.js
@@ -52,11 +52,38 @@ const filterMostPopulars = (arrayOfObjects) => {
 };
 
 // Funciones carrito
-const renderCart = (product) => {
-	const { name, img, price, subtitle } = product;
-	return;
+const renderCartList = (product) => {
+	const { id, name, img, price, subtitle, quantity } = product;
+	return `
+		<div class="items-cart">
+			<img class="image-simulate" src="${img}" alt="imagen de producto" />
+			<h3 class="items-cart-title">${name}</h3>
+			<span class="items-cart-second-title">${subtitle}</span>
+			<span class="items-cart-precio">$${price}</span>
+			<div class="buttons-pedido-container">
+				<button class="pedido-button-less" data-id=${id}>-</button>
+				<span class="pedido-value">${quantity}</span>
+				<button class="pedido-button-plus" data-id=${id}>+</button>
+			</div>
+		</div>
+	`;
 };
-// --> <-- //
+
+const cleanPrices = () => {
+	total.textContent = "--";
+	subtotal.textContent = "--";
+	envio.textContent = "--";
+};
+
+const cartRender = () => {
+	if (!cart.length) {
+		itemsCartSelected.innerHTML = "<p>Tu carrito está vacío :(</p>";
+		cleanPrices();
+		return;
+	}
+	itemsCartSelected.innerHTML = cart.map(renderCartList).join("");
+};
+// -->  Carrito  <-- //
 const init = () => {
 	cartNavIcon.addEventListener("click", showCartMenu);
 	closeButton.addEventListener("click", closeCartMenu);
@@ -66,5 +93,6 @@ const init = () => {
 		}
 	});
 	filterMostPopulars(productsArray);
+	cartRender();
 };
 init();
