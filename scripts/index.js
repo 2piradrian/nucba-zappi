8aee45112f7d4f261dd8d568bbf96a93be0876f0
Adrian Rodriguez <rodriguezcadr@gmail.com>
2022-10-20 15:15:29 -0300

    free shipping

--- scripts/index.js
+++ scripts/index.js
@@ -1,91 +1,85 @@
 // Funcion para guardar carrito en local storage
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 const saveToLocalStorage = (cart) => {
-  localStorage.setItem("cart", JSON.stringify(cart));
+	localStorage.setItem("cart", JSON.stringify(cart));
 };
 // Mostrar carrito de compras
 const showCartMenu = () => {
-  cartMenuContainer.style.display = "grid";
-  getPrices();
+	cartMenuContainer.style.display = "grid";
+	getPrices();
 };
 // Ocultar carrito de compras
 const closeCartMenu = () => {
-  cartMenuContainer.style.display = "none";
+	cartMenuContainer.style.display = "none";
 };
 
 const closeCartMenuToScroll = () => {
-  window.onscroll = () => {
-    if (document.documentElement.scrollTop > 30) {
-      closeCartMenu();
-    } else {
-      return;
-    }
-  };
+	window.onscroll = () => {
+		if (document.documentElement.scrollTop > 30) {
+			closeCartMenu();
+		} else {
+			return;
+		}
+	};
 };
 
 closeCartMenuToScroll();
 
 // Mostrar menu hamburguesa
 const openCloseBurguerMenu = () => {
-  if (navbarMenu.style.display === "flex") {
-    navbarMenu.style.display = "none";
-  } else {
-    navbarMenu.style.display = "flex";
-  }
+	if (navbarMenu.style.display === "flex") {
+		navbarMenu.style.display = "none";
+	} else {
+		navbarMenu.style.display = "flex";
+	}
 };
 
 //Mostrar navbar en resoluciones mayores a 900px de width
 const showNavBar = () => {
-  if (window.outerWidth > 900) {
-    navbarMenu.style.display = "flex";
-  } else navbarMenu.style.display = "none";
+	if (window.outerWidth > 900) {
+		navbarMenu.style.display = "flex";
+	} else navbarMenu.style.display = "none";
 };
 
 const renderNoPopulars = (noPopularsArray, current) => {
-  menuContainer.innerHTML += noPopularsArray[current]
-    .map(desestructuringPopulars)
-    .join("");
+	menuContainer.innerHTML += noPopularsArray[current].map(desestructuringPopulars).join("");
 };
 
 const showLessFunction = (array) => {
-  showButtonMore();
-  renderMostPopulars(array[0], 0);
+	showButtonMore();
+	renderMostPopulars(array[0], 0);
 };
 
 const showButtonMore = () => {
-  showMoreButton.classList.remove("disabled");
-  showLessButton.classList.add("disabled");
+	showMoreButton.classList.remove("disabled");
+	showLessButton.classList.add("disabled");
 };
 const showButtonLess = () => {
-  navigator.current = 0;
-  showMoreButton.classList.add("disabled");
-  showLessButton.classList.remove("disabled");
+	navigator.current = 0;
+	showMoreButton.classList.add("disabled");
+	showLessButton.classList.remove("disabled");
 };
 
 const showFourMore = () => {
-  const noPopularRest = productsArray.filter((e) => !e.popular);
-  const result = [];
-  const size = 4;
-  for (let i = 0; i <= noPopularRest.length; i += size) {
-    result.push(noPopularRest.slice(i, i + size));
-  }
-  renderNoPopulars(result, navigator.current);
-  navigator.current < result.length - 2
-    ? (navigator.current = navigator.current + 1)
-    : showButtonLess();
+	const noPopularRest = productsArray.filter((e) => !e.popular);
+	const result = [];
+	const size = 4;
+	for (let i = 0; i <= noPopularRest.length; i += size) {
+		result.push(noPopularRest.slice(i, i + size));
+	}
+	renderNoPopulars(result, navigator.current);
+	navigator.current < result.length - 2 ? (navigator.current = navigator.current + 1) : showButtonLess();
 };
 
 //funcion de renderizado de los más populares en HTML.
 const renderMostPopulars = (mostPopularsArray) => {
-  menuContainer.innerHTML = mostPopularsArray
-    .map(desestructuringPopulars)
-    .join("");
+	menuContainer.innerHTML = mostPopularsArray.map(desestructuringPopulars).join("");
 };
 
 // funcion solo para desestructurar los objs.
 const desestructuringPopulars = (popularObj) => {
-  const { id, name, img, price, subtitle, popular } = popularObj;
-  return `
+	const { id, name, img, price, subtitle, popular } = popularObj;
+	return `
 	<div class="itemContainer">
 		<h2 class="popular-h2 ${popular ? "" : "disabled"}">Popular</h2>
 		<img src="${img}" alt="imagen del item" srcset="" />
@@ -103,15 +97,15 @@ const desestructuringPopulars = (popularObj) => {
 
 // Filtra los populares y llama al render
 const filterMostPopulars = (arrayOfObjects) => {
-  const mostPopularFiltered = arrayOfObjects.filter((e) => e.popular);
-  renderMostPopulars(mostPopularFiltered);
-  return mostPopularFiltered;
+	const mostPopularFiltered = arrayOfObjects.filter((e) => e.popular);
+	renderMostPopulars(mostPopularFiltered);
+	return mostPopularFiltered;
 };
 
 // Renderiza los elementos en el carrito
 const renderCartList = (product) => {
-  const { id, name, img, price, subtitle, quantity } = product;
-  return `
+	const { id, name, img, price, subtitle, quantity } = product;
+	return `
 		<div class="items-cart">
 			<img class="image-simulate" src="${img}" alt="imagen de producto" />
 			<h3 class="items-cart-title">${name}</h3>
@@ -128,60 +122,57 @@ const renderCartList = (product) => {
 
 // Renderiza en el icono de carrito la cantidad de elementos que hay en el mismo
 const renderProductsCounterIcon = () => {
-  if (!cart.length) {
-    cleanProductsCartIcon();
-  } else {
-    let productsCounterArray = cart.map(desestructuringQuantity);
-    let totalProducts = productsCounterArray.reduce((a, b) => a + b, 0);
-    productsCounterIcon.style.display = "flex";
-    productsCounterIcon.innerHTML = `<p>${totalProducts}</p>`;
-  }
+	if (!cart.length) {
+		cleanProductsCartIcon();
+	} else {
+		let productsCounterArray = cart.map(desestructuringQuantity);
+		let totalProducts = productsCounterArray.reduce((a, b) => a + b, 0);
+		productsCounterIcon.style.display = "flex";
+		productsCounterIcon.innerHTML = `<p>${totalProducts}</p>`;
+	}
 };
 
 //Función que desestructura la cantidad de cada producto
 
 const desestructuringQuantity = (product) => {
-  const { quantity } = product;
-  return quantity;
+	const { quantity } = product;
+	return quantity;
 };
 
 // Vacia los precios en caso de no haber productos
 const cleanPrices = () => {
-  total.textContent = "--";
-  subtotal.textContent = "--";
-  envio.textContent = "--";
+	total.textContent = "--";
+	subtotal.textContent = "--";
+	envio.textContent = "--";
 };
 
 // Envia los productos a ser renderizados
 const cartRender = () => {
-  if (!cart.length) {
-    itemsCartSelected.innerHTML = "<p>Tu carrito está vacío :(</p>";
-    cleanPrices();
-    cleanProductsCartIcon();
-    return;
-  }
-  itemsCartSelected.innerHTML = cart.map(renderCartList).join("");
+	if (!cart.length) {
+		itemsCartSelected.innerHTML = "<p>Tu carrito está vacío :(</p>";
+		cleanPrices();
+		cleanProductsCartIcon();
+		return;
+	}
+	itemsCartSelected.innerHTML = cart.map(renderCartList).join("");
 };
 
 // Multiplica precio por cantidad y va acumulando para obtener el subtotal
 const getPrices = () => {
-  const precio = cart.reduce(
-    (acc, cur) => acc + Number(cur.price) * Number(cur.quantity),
-    0
-  );
-  let envio;
-  if (precio > 5000) {
-    envio = "Gratis";
-    total.textContent = precio;
-  } else {
-    envio = 500;
-    total.textContent = precio + envio;
-  }
-
-  subtotal.textContent = precio;
-  if (!cart.length) {
-    cleanPrices();
-  }
+	const precio = cart.reduce((acc, cur) => acc + Number(cur.price) * Number(cur.quantity), 0);
+	let envioPrice;
+	if (precio > 5000) {
+		envioPrice = "Gratis";
+		total.textContent = precio;
+	} else {
+		envioPrice = 500;
+		total.textContent = precio + envioPrice;
+	}
+	envio.textContent = envioPrice;
+	subtotal.textContent = precio;
+	if (!cart.length) {
+		cleanPrices();
+	}
 };
 
 // const closeMenuTargetDetect = (e) => {
@@ -193,93 +184,93 @@ const getPrices = () => {
 
 // Funcion que incrementa la cantidad del item en el carrito
 const incrementQuantity = (idProduct) => {
-  cart = cart.map((item) => {
-    if (item.id == idProduct) {
-      item.quantity++;
-      renderProductsCounterIcon();
-      return item;
-    } else {
-      return item;
-    }
-  });
-  deleteItem();
+	cart = cart.map((item) => {
+		if (item.id == idProduct) {
+			item.quantity++;
+			renderProductsCounterIcon();
+			return item;
+		} else {
+			return item;
+		}
+	});
+	deleteItem();
 };
 
 // Funcion que borra el item si la cantidad es cero
 const deleteItem = () => {
-  cart = cart.filter((item) => item.quantity);
-  getPrices();
-  saveToLocalStorage(cart);
-  cartRender();
-  renderProductsCounterIcon();
+	cart = cart.filter((item) => item.quantity);
+	getPrices();
+	saveToLocalStorage(cart);
+	cartRender();
+	renderProductsCounterIcon();
 };
 
 // Funcion que decrementa la cantidad del item en el carrito
 const decrementQuantity = (idProduct) => {
-  cart = cart.map((item) => {
-    if (item.id == idProduct) {
-      item.quantity--;
-      renderProductsCounterIcon();
-      if (!item.quantity) {
-        deleteItem(idProduct);
-      }
-      return item;
-    } else {
-      return item;
-    }
-  });
-  deleteItem();
+	cart = cart.map((item) => {
+		if (item.id == idProduct) {
+			item.quantity--;
+			renderProductsCounterIcon();
+			if (!item.quantity) {
+				deleteItem(idProduct);
+			}
+			return item;
+		} else {
+			return item;
+		}
+	});
+	deleteItem();
 };
 
 //Función que oculta el icono de cantidad de productos del carrito
 const cleanProductsCartIcon = () => {
-  productsCounterIcon.style.display = "none";
+	productsCounterIcon.style.display = "none";
 };
 
 // Funcion que alerta si un elemento ya está en el carrito
 const alertCart = () => {
-  return alert("El item seleccionado ya se encuentra dentro del carrito.");
+	return alert("El item seleccionado ya se encuentra dentro del carrito.");
 };
 
 // Añade el item a el carrito
 const addToCart = (itemSelected) => {
-  const withQuantity = { ...itemSelected, quantity: 1 };
-  cart.push(withQuantity);
-  getPrices();
-  cartRender();
-  saveToLocalStorage(cart);
-  renderProductsCounterIcon();
+	const withQuantity = { ...itemSelected, quantity: 1 };
+	cart.push(withQuantity);
+	getPrices();
+	cartRender();
+	saveToLocalStorage(cart);
+	renderProductsCounterIcon();
 };
 
 // Funcion que verifica antes de agregar el item
 const checkBeforeToAdd = (itemSelected) => {
 	if (!cart.length) {
-	  addToCart(itemSelected);
-	  return;
-  } else {
-	  const mapItemsId = cart.map((element)=>element.id);
-	  if(mapItemsId.includes(itemSelected.id)){
-		  alertCart();
-		  return;
-	  }
-  }
-	  addToCart(itemSelected);
-  };
+		addToCart(itemSelected);
+		return;
+	} else {
+		const mapItemsId = cart.map((element) => element.id);
+		if (mapItemsId.includes(itemSelected.id)) {
+			alertCart();
+			return;
+		}
+	}
+	addToCart(itemSelected);
+};
 
 // Funcion que obtiene el elemento y lo añade al carro o cambia la cantidad segun corresponda
 const getItemInfo = (e) => {
-  const idProduct = e.target.dataset.id;
-  const itemSelected = productsArray.filter((item) => item.id == idProduct)[0];
+	const idProduct = e.target.dataset.id;
+	const itemSelected = productsArray.filter((item) => item.id == idProduct)[0];
 
-  if (e.target.classList.contains("pedido-button-less")) {
-    return decrementQuantity(idProduct);
-  } else if (e.target.classList.contains("pedido-button-plus")) {
-    return incrementQuantity(idProduct);
-  } else if (!e.target.classList.contains("addToCart")) {
-    return;
-  }
+	if (e.target.classList.contains("pedido-button-less")) {
+		return decrementQuantity(idProduct);
+	} else if (e.target.classList.contains("pedido-button-plus")) {
+		return incrementQuantity(idProduct);
+	} else if (!e.target.classList.contains("addToCart")) {
+		return;
+	}
 
-  checkBeforeToAdd(itemSelected);
+	checkBeforeToAdd(itemSelected);
 };
 let randomNums = (arrayfor = []);
 
@@ -295,41 +286,33 @@ const randomProducts = () => {
 	menuContainer.innerHTML = "";
 	newPopular = productsArray.filter((objeto) => randomNums.includes(objeto.id));
 	console.log(newPopular);
-	renderNewPopular = newPopular.map(
-		(object) => (menuContainer.innerHTML += desestructuringPopulars(object))
-	);
+	renderNewPopular = newPopular.map((object) => (menuContainer.innerHTML += desestructuringPopulars(object)));
 };
 // Selecciona categoria y lo renderiza
 const renderMenu = (e) => {
-  const clickData = e.target.dataset.type;
-  if (clickData === "popular") {
-    randomProducts();
-  } else if (clickData) {
-    menuContainer.innerHTML = "";
-    const obtainProduct = productsArray.filter(
-      (objeto) => objeto.category === clickData
-    );
-    renderProduct = obtainProduct.map(
-      (object) => (menuContainer.innerHTML += desestructuringPopulars(object))
-    );
-  }
+	const clickData = e.target.dataset.type;
+	if (clickData === "popular") {
+		randomProducts();
+	} else if (clickData) {
+		menuContainer.innerHTML = "";
+		const obtainProduct = productsArray.filter((objeto) => objeto.category === clickData);
+		renderProduct = obtainProduct.map((object) => (menuContainer.innerHTML += desestructuringPopulars(object)));
+	}
 };
 
 const init = () => {
-  cartNavIcon.addEventListener("click", showCartMenu);
-  closeButton.addEventListener("click", closeCartMenu);
-  burguerIcon.addEventListener("click", openCloseBurguerMenu);
-  document.addEventListener("click", (e) => closeMenuTargetDetect(e));
-  showMoreButton.addEventListener("click", showFourMore);
-  showLessButton.addEventListener("click", () =>
-    showLessFunction(filterMostPopulars(productsArray))
-  );
-  filterMostPopulars(productsArray);
-  menuContainer.addEventListener("click", getItemInfo);
-  cartMenuContainer.addEventListener("click", getItemInfo);
-  window.addEventListener("resize", showNavBar);
-  document.addEventListener("click", renderMenu);
-  renderProductsCounterIcon();
+	cartNavIcon.addEventListener("click", showCartMenu);
+	closeButton.addEventListener("click", closeCartMenu);
+	burguerIcon.addEventListener("click", openCloseBurguerMenu);
+	document.addEventListener("click", (e) => closeMenuTargetDetect(e));
+	showMoreButton.addEventListener("click", showFourMore);
+	showLessButton.addEventListener("click", () => showLessFunction(filterMostPopulars(productsArray)));
+	filterMostPopulars(productsArray);
+	menuContainer.addEventListener("click", getItemInfo);
+	cartMenuContainer.addEventListener("click", getItemInfo);
+	window.addEventListener("resize", showNavBar);
+	document.addEventListener("click", renderMenu);
+	renderProductsCounterIcon();
 };
 cartRender();
 
